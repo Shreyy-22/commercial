d808f2a103822f0650e8d745b3b9d5dc5375d695
Prabin Kumar Mahato <prabinmahato414@gmail.com>
2024-07-05 00:35:14 +0545

    first commit

---
+++ commercial/src/Components/Assets/all_product.js
@@ -0,0 +1,163 @@
+import p1_img from '.commercial/src/Components/Assets/p1_img.jpg';
+import p2_img from '.commercial/src/Components/Assets/p2_img.jpg';
+import p3_img from ".commercial/src/Components/Assets/p3_img.jpg";
+import p4_img from "./commercial/src/Components/Assets/p4_img.jpg";
+import p5_img from "./commercial/src/Components/Assets/p5_img.jpg";
+import p6_img from "./commercial/src/Components/Assets/p6_img.jpg";
+import p7_img from "./commercial/src/Components/Assets/p7_img.jpg";
+import p8_img from "./commercial/src/Components/Assets/p8_img.jpg";
+import p9_img from "./commercial/src/Components/Assets/p9_img.jpg";
+import p10_img from "./commercial/src/Components/Assets/p10_img.jpg";
+import p11_img from "./commercial/src/Components/Assets/p11_img.jpg";
+import p12_img from '.commercial/src/Components/Assets/p12_img.jpg';
+import p13_img from '.commercial/src/Components/Assets/p13_img.jpg';
+import p14_img from '.commercial/src/Components/Assets/p14_img.jpg';
+
+
+let all_product=[
+    {
+        id:1,
+        name:'floral shirt printed design in half sleeves',
+        Category:"men",
+        image:p1_img,
+        new_price:400.0,
+        old_price:700.0,
+    },
+    {
+        id:2,
+        name:'floral shirt printed design in half sleeves',
+        Category:"men",
+        image:p2_img,
+        new_price:400.0,
+        old_price:700.0,
+    },
+    {
+        id:3,
+        name:'floral shirt printed design in half sleeves',
+        Category:"men",
+        image:p2_img,
+        new_price:400.0,
+        old_price:700.0,
+    },
+//     {
+//         id=2,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p2_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=3,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p3_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=4,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p4_img,
+//         new_price:400.0,
+//         old_pr5ice:700.0,
+
+//     },
+//     {
+//         id=5,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p5_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=6,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p6_img,
+//         new_price:400.0,
+//         old_price:700.0,
+//     },
+//     {
+//         id=7,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p7_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=8,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p8_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=9,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p9_img,
+//         new_price:400.0,
+//         old_pr5ice:700.0,
+
+//     },
+//     {
+//         id=10,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p10_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=11,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p11_img,
+//         new_price:400.0,
+//         old_price:700.0,
+//     },
+//     {
+//         id=12,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p12_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=13,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p13_img,
+//         new_price:400.0,
+//         old_price:700.0,
+
+//     },
+//     {
+//         id=14,
+//         name:floral shirt printed design in half sleeves",
+//         Category:"men";
+//         image:p14_img,
+//         new_price:400.0,
+//         old_pr5ice:700.0,
+
+//     },
+    
+ ]
+
+
+
+
+
---
+++ commercial/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <div classname="navbar">
+      <h1>navbar</h1>
+    </div>
+  )
+}
+
+export default Navbar
